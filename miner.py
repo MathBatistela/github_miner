da5559b4b573a938bdc8dc77a1e3c441796eb483
Matheus Batistela <matheus.batistela@dataway.info>
2022-05-12 18:48:43 -0300

    feat: adds new miner tool

--- miner.py
+++ miner.py
@@ -1,45 +1,49 @@
 from python_graphql_client import GraphqlClient
-import config
-
-headers = {"Authorization": f"Bearer {config.GITHUB_API_TOKEN}"}
-client = GraphqlClient(endpoint=config.GITHUB_API_URL, headers=headers)
 
 
-def make_query(owner, name, after_cursor=None):
-    return """
-        query {
-        repository (owner: OWNER, name: NAME) {
-            pullRequests(first: 100, after: AFTER){
-                nodes {
-                author{
-                    login
+class PullRequestsIterator:
+    def __init__(self, api_url: str, token: str):
+        headers = {"Authorization": f"Bearer {token}"}
+        self.client = GraphqlClient(endpoint=api_url, headers=headers)
+
+    @staticmethod
+    def __query__(owner: str, name: str, after_cursor=None):
+        return (
+            """
+            query {
+                repository(owner: OWNER, name: NAME) {
+                    pullRequests(first: 100, after: AFTER) {
+                        nodes {
+                            author {
+                                login
+                            }
+                        }
+                        pageInfo {
+                            hasNextPage
+                            endCursor
+                        }
+                    }
                 }
-                }
-            pageInfo {
-                hasNextPage
-                endCursor
             }
-            }
-        }
-        }
-    """.replace(
-        "AFTER", '"{}"'.format(after_cursor) if after_cursor else "null"
-    ).replace(
-        "OWNER", '"{}"'.format(owner)
-    ).replace(
-        "NAME", '"{}"'.format(name)
-    )
+        """.replace(
+                "AFTER", '"{}"'.format(after_cursor) if after_cursor else "null"
+            )
+            .replace("OWNER", '"{}"'.format(owner))
+            .replace("NAME", '"{}"'.format(name))
+        )
 
+    def get_iterator(self, repo_owner: str, repo_name: str):
+        has_next_page = True
+        after_cursor = None
+        while has_next_page:
+            data = self.client.execute(
+                query=self.__query__(repo_owner, repo_name, after_cursor),
+            )
+            has_next_page = data["data"]["repository"]["pullRequests"]["pageInfo"][
+                "hasNextPage"
+            ]
+            after_cursor = data["data"]["repository"]["pullRequests"]["pageInfo"][
+                "endCursor"
+            ]
 
-has_next_page = True
-after_cursor = None
-while has_next_page:
-    data = client.execute(
-        query=make_query("vuejs", "vue", after_cursor),
-    )
-
-    print(data)
-    has_next_page = data["data"]["repository"]["pullRequests"]["pageInfo"]["hasNextPage"]
-    after_cursor = data["data"]["repository"]["pullRequests"]["pageInfo"]["endCursor"]
-    print(has_next_page)
-    print(after_cursor)
+            yield data
